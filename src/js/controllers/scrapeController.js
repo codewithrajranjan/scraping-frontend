f567a0f7067da7ec35a21cb91a410284168f5de1
Raj Ranjan <raj.ranjan@alepo.com>
2018-08-30 18:36:38 +0000

    changes

--- src/js/controllers/scrapeController.js
+++ src/js/controllers/scrapeController.js
@@ -1,16 +1,31 @@
 //chart.js
 angular
-.module('app')
-.controller('ScrapeCtrl', ScrapeCtrl)
+    .module('app')
+    .controller('ScrapeCtrl', ScrapeCtrl)
 
 ScrapeCtrl.$inject = ['$scope','$http'];
 function ScrapeCtrl($scope,$http) {
-  $scope.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    $scope.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
-    $http.get('http://localhost:5000/api/v1.0/scrape')
-    .then(function(data){
-        $scope.blogList = data.data
-    }).catch(function(err){
-        console.log(err)
-    })
+    init()
+    function init(){
+        $http.get('http://localhost:9002/api/v1.0/blogs')
+            .then(function(data){
+                $scope.blogList = data.data
+                $scope.totalNewPost = $scope.blogList.length
+            }).catch(function(err){
+                console.log(err)
+            })
+
+    }
+
+
+    $scope.scrape = function(){
+        $http.post('http://localhost:9002/api/v1.0/scrape')
+            .then(function(data){
+                init()
+            }).catch(function(err){
+                console.log(err)
+            })
+    }
 }
