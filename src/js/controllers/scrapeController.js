f8007645570a3a2444d446ac5731170551a519f0
Raj Ranjan <raj.ranjan@alepo.com>
2018-09-01 09:44:37 +0000

    changes

--- src/js/controllers/scrapeController.js
+++ src/js/controllers/scrapeController.js
@@ -7,9 +7,15 @@ ScrapeCtrl.$inject = ['$scope','$http'];
 function ScrapeCtrl($scope,$http) {
     $scope.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
-    init()
-    function init(){
-        $http.get('http://localhost:9002/api/v1.0/blogs')
+    // function declarations 
+    $scope.updateBlogStatus = updateBlogStatus;
+
+    $scope.getPosts = getPosts
+        
+    getPosts('new')
+
+    function getPosts(postStatus){
+        $http.get('http://localhost:9002/api/v1.0/blogs?status='+postStatus)
             .then(function(data){
                 $scope.blogList = data.data
                 $scope.totalNewPost = $scope.blogList.length
@@ -18,12 +24,21 @@ function ScrapeCtrl($scope,$http) {
             })
 
     }
+    function updateBlogStatus(blogData,statusCode){
+        var id = blogData._id.$oid
+        $http.post('http://localhost:9002/api/v1.0/blog/'+id+'/status/'+statusCode)
+            .then(function(data){
+                getPosts('new')
+            }).catch(function(err){
+                console.log(err)
+            })
+    }
 
 
     $scope.scrape = function(){
         $http.post('http://localhost:9002/api/v1.0/scrape')
             .then(function(data){
-                init()
+                getPosts('new')
             }).catch(function(err){
                 console.log(err)
             })
